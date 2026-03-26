a5d587909e9f53b3d063c0275e63854d8ecfa782
Bauger Cillian <baugercillianpro@gmail.com>
2026-03-26 15:45:25 +0100

    Remove docstrings from ClinicalCaseManager methods
    Removed docstrings from methods in ClinicalCaseManager.

--- public/auth_app/managers/ClinicalCaseManager.py
+++ public/auth_app/managers/ClinicalCaseManager.py
@@ -2,9 +2,7 @@ from django.db import models
 
 class ClinicalCaseManager(models.Manager):
     def get_cases_for_study_level(self, level):
-        """Get cases for a specific study level"""
         return self.filter(study_level__iexact=level)
         
     def get_cases_with_standardized_patient(self):
-        """Get patient were patient with interaction"""
         return self.filter(has_standardized_patient=True)
