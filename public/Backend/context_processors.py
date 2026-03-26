0d05a0ff0aa9e57c036aa9f7e7586a5e4e17be02
ADRO4545 <adrugeault19@gmail.com>
2026-03-26 10:04:54 +0100

    base de données ajouter

--- public/Backend/context_processors.py
+++
@@ -1,5 +0,0 @@
-def template(request):
-    is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest' # on verifie si la requete viens du router
-    return {
-        'base_template': 'partial.html' if is_ajax else 'index.html' # si la requete viens du router on renvoi partial.html sinon index.html
-    }
